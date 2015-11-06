5fba58c8739c5b2bd2f7008acde95a1b9efbf027
Ramon Lapenta <ramon@cyber-duck.co.uk>
2015-11-06 15:51:48 +0000

    Added a new functions file with some basic functions to start Fixed a bug on desktop nested columns

--- Gulpfile.js
+++ Gulpfile.js
@@ -2,23 +2,19 @@ var gulp = require('gulp'),
     sourcemaps = require('gulp-sourcemaps'),
     sass = require('gulp-sass'),
     minify = require('gulp-minify-css'),
-    concat = require('gulp-concat'),
-    uglify = require('gulp-uglify'),
     watch = require('gulp-watch');
 
 // Modify these variables according to your project
 var config = {
     scssDir: './scss',
-    jsDir: './js',
     cssDir: './css'
 };
 
-gulp.task('default', ['style', 'js']);
+gulp.task('default', ['style']);
 
 // Compile Sass files
 gulp.task('style', function() {
     gulp.src(config.scssDir + '/*.scss')
-    .pipe(sourcemaps.init())
     .pipe(sass())
     .pipe(minify())
     .pipe(gulp.dest(config.cssDir));
@@ -32,43 +28,15 @@ gulp.task('style-dev', function() {
 });
 
 
-// Concatenate JS files
-gulp.task('js', function() {
-    return gulp.src(config.jsDir + '/src/*.js')
-    .pipe(concat('scripts.js'))
-    .pipe(gulp.dest(config.jsDir));
-});
-
-
-// Compress JS file
-gulp.task('compress', function() {
-    return gulp.src(config.jsDir + '/scripts.js')
-    .pipe(uglify())
-    .pipe(gulp.dest(config.jsDir + '/min'));
-});
-gulp.task('compress-dev', function() {
-    return gulp.src(config.jsDir + '/scripts.js')
-    .pipe(gulp.dest(config.jsDir + '/min'));
-});
 
 // Production
 gulp.task('build', function() {
     gulp.start('style');
-    gulp.start('compress');
 });
 
 // Watch for changes
 gulp.task('watch', function() {
-    watch(config.scssDir + '/**/*.scss', function () {
+    watch(config.scssDir + '/**/*.scss', function() {
         gulp.start('style-dev');
     });
-
-    watch(config.jsDir + '/src/*.js', function () {
-        gulp.start('js');
-    });
-
-    watch(config.jsDir + '/scripts.js', function () {
-        gulp.start('compress-dev');
-    });
-
 });
