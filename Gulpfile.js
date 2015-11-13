e05e8d1ef28f2f4f9b0e745a8a24fcb9bdab5a39
Ramon Lapenta <ramon@cyber-duck.co.uk>
2015-11-13 09:25:37 +0000

    Updated copy

--- Gulpfile.js
+++ Gulpfile.js
@@ -2,58 +2,51 @@ var gulp = require('gulp'),
     sourcemaps = require('gulp-sourcemaps'),
     sass = require('gulp-sass'),
     minify = require('gulp-minify-css'),
-    concat = require('gulp-concat'),
-    uglify = require('gulp-uglify'),
-    watch = require('gulp-watch');
+    watch = require('gulp-watch'),
+    scsslint = require('gulp-scss-lint');
 
 // Modify these variables according to your project
 var config = {
     scssDir: './scss',
-    jsDir: './js',
     cssDir: './css'
 };
 
-gulp.task('default', ['style', 'js']);
+gulp.task('default', ['style']);
+
 
 // Compile Sass files
 gulp.task('style', function() {
     gulp.src(config.scssDir + '/*.scss')
+    .pipe(sass())
+    .pipe(minify())
+    .pipe(gulp.dest(config.cssDir));
+});
+gulp.task('style-dev', function() {
+    gulp.src(config.scssDir + '/*.scss')
     .pipe(sourcemaps.init())
     .pipe(sass())
-    //.pipe(minify())
-    .pipe(sourcemaps.write('maps'))
+    .pipe(sourcemaps.write())
     .pipe(gulp.dest(config.cssDir));
 });
 
-
-// Concatenate JS files
-gulp.task('js', function() {
-    return gulp.src(config.jsDir + '/src/*.js')
-    .pipe(concat('scripts.js'))
-    .pipe(gulp.dest(config.jsDir));
+ 
+gulp.task('lint', function() {
+  return gulp.src(config.scssDir + '/*.scss')
+    .pipe(scsslint({
+        'config': 'lint.yml',
+    }));
 });
 
 
-// Compress JS file
-gulp.task('compress', function() {
-    return gulp.src(config.jsDir + '/scripts.js')
-    .pipe(uglify())
-    .pipe(gulp.dest(config.jsDir + '/min'));
+// Production
+gulp.task('build', function() {
+    gulp.start('style');
 });
 
 
 // Watch for changes
 gulp.task('watch', function() {
-    watch(config.scssDir + '/**/*.scss', function () {
-        gulp.start('style');
+    watch(config.scssDir + '/**/*.scss', function() {
+        gulp.start('style-dev');
     });
-
-    //watch(config.jsDir + '/src/*.js', function () {
-//    //    gulp.start('js');
-//    //});
-//
-    //watch(config.jsDir + '/scripts.js', function () {
-    //    gulp.start('compress');
-    //});
-
 });
