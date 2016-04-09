95bdd596643b987a9ff64a737cd0193236a12880
Ramon Lapenta <ramono@gmail.com>
2016-04-09 20:46:36 +0100

    New release:  - Added autoprefixer to the build system  - Added a mixin for media queries (respond-to, needs documentation)  - Cleaned files  - Fixed Gulpfile  - Updated gulp and plugins  - Added minified version of compiled css  - Moved sourcemaps to their own folder

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,8 +1,10 @@
 var gulp = require('gulp'),
     sourcemaps = require('gulp-sourcemaps'),
     sass = require('gulp-sass'),
-    minify = require('gulp-minify-css'),
+    cssnano = require('gulp-cssnano'),
     watch = require('gulp-watch'),
+    autoprefixer = require('gulp-autoprefixer'),
+    rename = require('gulp-rename'),
     scsslint = require('gulp-scss-lint');
 
 // Modify these variables according to your project
@@ -13,40 +15,50 @@ var config = {
 
 gulp.task('default', ['style']);
 
-
 // Compile Sass files
-gulp.task('style', function() {
+gulp.task('style', function () {
+    'use strict';
     gulp.src(config.scssDir + '/*.scss')
-    .pipe(sass())
-    .pipe(minify())
-    .pipe(gulp.dest(config.cssDir));
+        .pipe(sass())
+        .pipe(autoprefixer({browsers: ['last 2 versions', 'ie >= 9']}))
+        .pipe(cssnano())
+        .pipe(gulp.dest(config.cssDir));
 });
-gulp.task('style-dev', function() {
+gulp.task('style-dev', function () {
+    'use strict';
     gulp.src(config.scssDir + '/*.scss')
-    .pipe(sourcemaps.init())
-    .pipe(sass())
-    .pipe(sourcemaps.write())
-    .pipe(gulp.dest(config.cssDir));
+        .pipe(sourcemaps.init())
+        .pipe(sass({outputStyle: "expanded"}))
+        .pipe(autoprefixer({browsers: ['last 2 versions', 'ie >= 9']}))
+        .pipe(sourcemaps.write('maps'))
+        .pipe(gulp.dest(config.cssDir));
 });
 
- 
-gulp.task('lint', function() {
-  return gulp.src(config.scssDir + '/*.scss')
-    .pipe(scsslint({
-        'config': 'lint.yml',
-    }));
+gulp.task('prod', ['style-dev'], function () {
+    return gulp.src(config.cssDir + '/style.css')
+        .pipe(cssnano())
+        .pipe(rename('style.min.css'))
+        .pipe(gulp.dest(config.cssDir));
 });
 
+gulp.task('lint', function () {
+    'use strict';
+    return gulp.src(config.scssDir + '/*.scss')
+        .pipe(scsslint({
+            config: 'lint.yml'
+        }));
+});
 
 // Production
-gulp.task('build', function() {
-    gulp.start('style');
+gulp.task('build', function () {
+    'use strict';
+    gulp.start('prod');
 });
 
-
 // Watch for changes
-gulp.task('watch', function() {
-    watch(config.scssDir + '/**/*.scss', function() {
+gulp.task('watch', function () {
+    'use strict';
+    watch(config.scssDir + '/**/*.scss', function () {
         gulp.start('style-dev');
     });
 });
