159a8977b6027b18203229c715fcea8da5136c4c
Ramon Lapenta <ramono@gmail.com>
2017-05-18 09:16:33 -0600

    Updated provided build system and changed css lint
    Stylelint is now used with a set of rules that matches what we use at Cyber-Duck

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,66 +1,146 @@
-var gulp = require('gulp'),
-    sourcemaps = require('gulp-sourcemaps'),
-    sass = require('gulp-sass'),
-    cssnano = require('gulp-cssnano'),
-    watch = require('gulp-watch'),
-    autoprefixer = require('gulp-autoprefixer'),
-    rename = require('gulp-rename'),
-    scsslint = require('gulp-scss-lint');
+/*
+ * Main Gulpfile for any type of projects
+ * Cyber-Duck Ltd - www.cyber-duck.co.uk
+ */
 
-// Modify these variables according to your project
-var config = {
+const gulp = require('gulp'),
+    del = require('del'),
+    autoprefixer = require('gulp-autoprefixer'),
+    minify = require('gulp-cssnano'),
+    rename = require("gulp-rename"),
+    sass = require('gulp-sass'),
+    sourcemaps = require('gulp-sourcemaps'),
+    watch = require('gulp-watch'),
+    csslint = require('gulp-stylelint');
+
+
+/*
+ * Main configuration object
+ */
+const config = {
     scssDir: './scss',
-    cssDir: './css'
+    cssDir: './css',
 };
 
-gulp.task('default', ['style']);
 
-// Compile Sass files
-gulp.task('style', function () {
+
+/*
+ * Compile Sass for development
+ * with sourcemaps and not minified
+ */
+gulp.task('style-dev', () => {
     'use strict';
-    gulp.src(config.scssDir + '/*.scss')
+    return gulp.src(config.scssDir + '/*.scss')
+        .pipe(sourcemaps.init())
         .pipe(sass())
         .on('error', sass.logError)
-        .pipe(autoprefixer({browsers: ['last 2 versions', 'ie >= 9']}))
-        .pipe(cssnano())
-        .pipe(gulp.dest(config.cssDir));
-});
-gulp.task('style-dev', function () {
-    'use strict';
-    gulp.src(config.scssDir + '/*.scss')
-        .pipe(sourcemaps.init())
-        .pipe(sass({outputStyle: "expanded"}))
-        .on('error', sass.logError)
-        .pipe(autoprefixer({browsers: ['last 2 versions', 'ie >= 9']}))
+        .pipe(autoprefixer({browsers: ['last 2 versions'], cascade: false}))
         .pipe(sourcemaps.write('maps'))
         .pipe(gulp.dest(config.cssDir));
 });
 
-gulp.task('prod', ['style-dev'], function () {
-    return gulp.src(config.cssDir + '/style.css')
-        .pipe(cssnano())
-        .pipe(rename('style.min.css'))
+
+
+/*
+ * Compile Sass for production
+ * with no sourcemaps and minified
+ */
+gulp.task('style', () => {
+    'use strict';
+    return gulp.src(config.scssDir + '/*.scss')
+        .pipe(sass())
+        .on('error', sass.logError)
+        .pipe(autoprefixer({browsers: ['last 2 versions'], cascade: false}))
+        .pipe(minify({
+            zindex: false, 
+            discardComments: {
+                removeAll: true
+            }
+        }))
         .pipe(gulp.dest(config.cssDir));
 });
 
-gulp.task('lint', function () {
-    'use strict';
-    return gulp.src(config.scssDir + '/*.scss')
-        .pipe(scsslint({
-            config: 'lint.yml'
-        }));
+
+
+/*
+ * Lint CSS/Scss using Style Lint
+ */
+gulp.task('lint-css', function lintCssTask() {
+    return gulp.src(config.scssDir + '/**/*.scss')
+        .pipe(csslint({
+            failAfterError: true,
+            reportOutputDir: 'reports/lint',
+            reporters: [
+                {formatter: 'verbose', console: true},
+                {formatter: 'json', save: 'report.json'},
+            ],
+        debug: true
+    }));
 });
 
-// Production
-gulp.task('build', function () {
+
+/*
+ * Clean non used JS files, and sourcemaps for production
+ */
+gulp.task('clean', ['style', 'compress'], () => {
     'use strict';
-    gulp.start('prod');
+    del(config.cssDir + '/maps/*');
+    del(config.cssDir + '/maps/');
 });
 
-// Watch for changes
-gulp.task('watch', function () {
+
+
+/*
+ * Optimise images uploaded in the CMS
+ */
+gulp.task('imgoptim', (cb) => {
     'use strict';
-    watch(config.scssDir + '/**/*.scss', function () {
+    return gulp.src([
+        config.imgSrc + '/**/*.png',
+        config.imgSrc + '/**/*.jpg',
+        config.imgSrc + '/**/*.gif',
+        config.imgSrc + '/**/*.jpeg'
+    ])
+        .pipe(imageop({
+            optimizationLevel: 5,
+            progressive: true,
+            interlaced: true
+        }))
+        .pipe(gulp.dest(config.imgSrc)).on('end', cb).on('error', cb);
+});
+
+
+
+/*
+ * Production task
+ * Compiles Sass with no sourcemaps, minifies CSS
+ * Compress JS
+ * Clean sourcemaps and uncompressed JS
+*/
+gulp.task('build', () => {
+    'use strict';
+    gulp.start('style');
+    gulp.start('clean');
+});
+
+
+
+/*
+ * Development mode, watching for changes
+ * This mode creates non compressed CSS, non compressed JS
+ * and creates sourcemaps.
+*/
+gulp.task('watch', () => {
+    'use strict';
+    // Development task for compiling Sass
+    watch(config.scssDir + '/**/*.scss', () => {
         gulp.start('style-dev');
     });
 });
+
+
+
+/*
+ * Default task, runs the build task
+ */
+gulp.task('default', ['build']);
